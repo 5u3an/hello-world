dd1e542cca40eb1dde55d7f813b0755fa13e7121
Nayan47 <magarnayan539@gmail.com>
2019-08-12 18:02:24 +0545

    classwork

---
+++ cw.py
@@ -0,0 +1,40 @@
+
+updown = 0
+
+leftright = 0
+
+# try:
+while True:
+
+    dir_step = input("direction steps: ")
+
+    if not dir_step:
+        break
+
+    dir_step = dir_step.split(' ')
+    direction = dir_step[0].upper()
+   
+    try:
+        step = int(dir_step[1])
+        if direction == "UP":
+
+            updown = updown + step
+    
+        elif direction == "DOWN" :
+    
+            updown = updown - step
+    
+        elif direction == "LEFT":
+    
+            leftright = leftright - step
+    
+        elif direction == "RIGHT":
+    
+            leftright = leftright + step
+    except:
+        pass
+    
+    
+print(f"Distance: {int((updown**2+leftright**2)**(1/2))}")
+# except:
+# print("Please use correct command.")
